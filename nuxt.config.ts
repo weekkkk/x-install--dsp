a84f14f6a66812eeca603b4551ec507d0e38850b
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-02-28 22:09:51 +0200

    configurate nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+  modules: ["@nuxt/ui"],
+  tailwindcss: {
+    cssPath: "~/src/app/assets/css/tailwind.css",
+  },
+  ui: {
+    global: true,
+  },
+  colorMode: {
+    preference: "dark",
+  },
+});
