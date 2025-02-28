9787ca4c763ca4c95460c596532b856478d9ffaa
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-02-28 22:44:49 +0200

    fix font styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,11 @@ export default <Partial<Config>>{
     extend: {
       fontSize: {
         base: ["1rem", 1.2],
-        lg: ["1.6rem", 1.2],
-        xl: ["2rem", 1.2],
-        "2xl": ["2.4rem", 1],
-        "3xl": ["3.5rem", 1.2],
+        lg: ["1.4rem", 1.2],
+        xl: ["1.6rem", 1.2],
+        "2xl": ["2rem", 1.2],
+        "3xl": ["2.4rem", 1],
+        "4xl": ["3.5rem", 1.2],
       },
     },
   },
