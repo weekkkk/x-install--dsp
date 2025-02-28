25660909c4a1977112f03663f3f71b4ee088eebe
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-02-28 22:38:20 +0200

    init font styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,14 @@ import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: ["1rem", 1.2],
+        lg: ["1.6rem", 1.2],
+        xl: ["2rem", 1.2],
+        "2xl": ["2.4rem", 1],
+        "3xl": ["3.5rem", 1.2],
+      },
+    },
   },
 };
