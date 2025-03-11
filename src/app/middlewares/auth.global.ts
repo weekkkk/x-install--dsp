cb701b68ddb0a40a48fbe731310da299d96cf558
Nikita Nedelko <93500964+weekkkk@users.noreply.github.com>
2025-03-12 01:36:19 +0200

    fix

--- src/app/middlewares/auth.global.ts
+++ src/app/middlewares/auth.global.ts
@@ -1,6 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   const auth = useState("auth");
+  const user = useState<AuthResDto["user"]>("user");
   if (auth.value === undefined) return;
 
   if (!auth.value && to.path !== "/login") return navigateTo("/login");
+  if (to.path === "/users" && user.value.role !== "Admin")
+    return navigateTo("/");
+  if (to.path === "/" && user.value.role === "Admin" && !to.query.user)
+    return navigateTo({
+      path: "/users",
+      query: {
+        mode: "view"
+      }
+    });
 });
