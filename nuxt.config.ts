9083fc3202bc2a2f384406a1a75bd218ea59c474
Nikita Nedelko <93500964+weekkkk@users.noreply.github.com>
2025-05-12 01:42:57 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/eslint", "@nuxthub/core"],
+  modules: ["@nuxt/image", "@nuxt/ui", "@nuxt/eslint",
+    // "@nuxthub/core"
+  ],
   components: {
     dirs: [
       {
